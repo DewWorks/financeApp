e0fe4116dca751712b8f3c378148b84a85b92adb
joaoito <joaovictorpfr@gmail.com>
2024-11-06 17:57:07 -0300

    test connectionDb string in committed

--- src/db/connectionDb.ts
+++ src/db/connectionDb.ts
@@ -1,14 +1,12 @@
 /* eslint-disable prefer-const */
 import { MongoClient } from 'mongodb';
 
-const uri: string = process.env.MONGODB_URI || "mongodb://localhost:27017";
+const uri: string = process.env.MONGODB_URI || "MONGODB_URI=mongodb+srv://joaovictorpfr:FgzSSwobypYQCI4U@financeapp.qgkzu.mongodb.net/?retryWrites=true&w=majority&appName=financeApp";
 
 const options = {};
 
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
-  // Em modo de produção, não use uma variável global
-  // eslint-disable-next-line prefer-const
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
 
