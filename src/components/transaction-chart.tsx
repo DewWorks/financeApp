4e21c70c51efef5fb0878883e2d5d175f49dc6c5
joaoito <joaovictorpfr@gmail.com>
2024-11-06 14:08:10 -0300

    fix errors and dependences deploy

--- src/components/transaction-chart.tsx
+++ src/components/transaction-chart.tsx
@@ -1,20 +1,24 @@
-import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts"
-import { ITransaction } from "../interfaces/ITransaction"
+import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis, Cell } from "recharts";
+import { ITransaction } from "../interfaces/ITransaction";
 
 export function TransactionChart({ transactions }: { transactions: ITransaction[] }) {
   const data = transactions.map(t => ({
     name: t.description,
     value: t.amount,
     color: t.type === 'income' ? '#10B981' : '#EF4444'
-  }))
+  }));
 
   return (
     <ResponsiveContainer width="100%" height={300}>
       <BarChart data={data}>
         <XAxis dataKey="name" />
         <YAxis />
-        <Bar dataKey="value" fill={(entry) => entry.color} />
+        <Bar dataKey="value">
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={entry.color} />
+          ))}
+        </Bar>
       </BarChart>
     </ResponsiveContainer>
-  )
+  );
 }
