8cfa0aceada149348dcb568ece26744b5fb86355
joaoito <joaovictorpfr@gmail.com>
2024-11-15 14:59:57 -0300

    layout: a better layout for a title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,11 +2,9 @@ import { Wallet } from "lucide-react";
 
 export function Title(){
     return(
-        <div className="flex">
-              <div className="flex-shrink-0 flex items-center">
+              <div className="flex items-center justify-center">
                 <Wallet className="h-12 w-12 text-blue-600" />
-                <span className="m-4 text-5xl font-bold text-gray-900">FinançasPro</span>
+                <span className="m-4 text-5xl font-bold text-gray-900">FinancePro</span>
               </div>
-            </div>
     )
 }
