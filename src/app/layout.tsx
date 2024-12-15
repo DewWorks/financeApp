737938378b80dee2f4e775891bd4d4972ac51e2a
joaoito <joaovictorpfr@gmail.com>
2024-12-15 16:43:09 -0300

    import react

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { AuthPopup } from "@/components/ui/molecules/AuthPopup";
+import React from "react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
