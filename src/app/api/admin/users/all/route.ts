5db549eb4e4818c7d6e0df27f93136efd958e0fa
joaoito <joaovictorpfr@gmail.com>
2025-05-22 00:01:06 -0300

    FEAT - allUsers get with admin

---
+++ src/app/api/admin/users/all/route.ts
@@ -0,0 +1,28 @@
+import { getMongoClient } from "@/db/connectionDb"
+import { NextResponse } from "next/server"
+import { getUserIdFromToken } from "@/app/functions/getUserId"
+
+export async function GET() {
+    try {
+
+        const client = await getMongoClient()
+        const db = client.db("financeApp")
+
+        // Fetch all users but exclude sensitive information like passwords
+        const users = await db.collection("users").find({}).project({ password: 0 }).toArray()
+
+        return NextResponse.json(users)
+    } catch (error) {
+        console.error("Get users error:", error)
+
+        if (error instanceof Error && error.name === "AuthError") {
+            return NextResponse.json({ error: "Unauthorized" }, { status: 401 })
+        }
+
+        if (error instanceof Error && error.name === "MongoNetworkError") {
+            return NextResponse.json({ error: "Database connection error" }, { status: 503 })
+        }
+
+        return NextResponse.json({ error: "Internal server error" }, { status: 500 })
+    }
+}
