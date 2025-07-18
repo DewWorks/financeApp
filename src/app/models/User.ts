8af5b6fec12a7cfb4ea3f1be9f4dc3cca87c9cf2
João Victor <joaovictorpfr@gmail.com>
2025-07-18 13:12:37 -0300

    FEAT - esqueceu a senha reset de verificacao de email

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,15 +1,31 @@
-import { IUser } from "@/interfaces/IUser";
+import { IUser, IUserTypesVerification } from "@/interfaces/IUser";
 import mongoose, { Schema } from "mongoose";
 
 const UserSchema = new Schema<IUser>({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    cel: String,
+    cel: {
+        type: [String],
+        required: true,
+        validate: {
+            validator: function (value: string[]) {
+                return Array.isArray(value) && value.length > 0;
+            },
+            message: 'Pelo menos um número de celular é obrigatório',
+        },
+    },
     address: String,
     cpf: String,
     tutorialGuide: { type: Boolean, default: true }, 
-    executeQuery: { type: Boolean, default: false }
+    executeQuery: { type: Boolean, default: false },
+    verification: {
+        code: String,
+        type: { type: IUserTypesVerification }, // 'reset-password' | 'verify-number' | 'mfa-login'
+        channels: [String],
+        expiresAt: Date,
+        verified: { type: Boolean, default: false }
+    }
 });
 
 export const User = mongoose.model<IUser>("User", UserSchema);
--- src/interfaces/IUser.ts
+++ src/interfaces/IUser.ts
@@ -5,9 +5,24 @@ export interface IUser {
     name: string;
     email: string;
     password: string;
-    cel?: string;
+    cel?: string[];
     address?: string;
     cpf?: string;
     tutorialGuide?: boolean;
     executeQuery?: boolean;
+    verification?: IUserVerification
+}
+
+export interface IUserVerification {
+    code: string
+    type: IUserTypesVerification
+    channels: ('email' | 'whatsapp')[]
+    expiresAt: Date
+    verified: boolean
+}
+
+export enum IUserTypesVerification {
+    'reset-password',
+    'verify-number',
+    'mfa-login'
 }
