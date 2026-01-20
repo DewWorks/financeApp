a72073a4e88a14afd0e8821ed06c239e92ac389f
JoaoIto <joaovictorpfr@gmail.com>
2026-01-20 19:01:51 -0300

    fix tandalone and force dynamic

--- src/app/api/doc/route.ts
+++ src/app/api/doc/route.ts
@@ -1,6 +1,8 @@
 import { getApiDocs } from "@/lib/swagger";
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
     const spec = await getApiDocs();
     return NextResponse.json(spec);
