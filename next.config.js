459497d97d2907b90a5e5e0e9e550a1c3eb09cc4
JoaoIto <joaovictorpfr@gmail.com>
2026-01-20 19:10:26 -0300

    next config webpack

--- next.config.js
+++ next.config.js
@@ -2,12 +2,6 @@
 const nextConfig = {
     output: "standalone",
     reactStrictMode: false,
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            config.resolve.fallback = { fs: false, canvas: false };
-        }
-        return config;
-    },
     typescript: {
         ignoreBuildErrors: true,
     }
