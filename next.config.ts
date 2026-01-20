5c25a177428605cd50cfa7a4de4d0546284c9b50
JoaoIto <joaovictorpfr@gmail.com>
2026-01-20 15:11:36 -0300

    fix: output standalone

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    output: "standalone",
     reactStrictMode: false,
     webpack: (config, { isServer }) => {
         if (!isServer) {
