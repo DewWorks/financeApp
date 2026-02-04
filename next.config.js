da8ec9e246dce1ef685f059039b0bf11526918e5
JoaoIto <joaovictorpfr@gmail.com>
2026-02-04 15:51:31 -0300

    HOTFIX: tests for next vercel Access-Control-Allow-Credentials

--- next.config.js
+++ next.config.js
@@ -15,11 +15,16 @@ const nextConfig = {
     webpack: (config) => {
         return config;
     },
+    trailingSlash: false,
     async headers() {
         return [
             {
                 source: '/:path*',
                 headers: [
+                    { key: 'Access-Control-Allow-Credentials', value: 'true' },
+                    { key: 'Access-Control-Allow-Origin', value: '*' },
+                    { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+                    { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
                     { key: 'X-DNS-Prefetch-Control', value: 'on' },
                     { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' },
                     { key: 'X-XSS-Protection', value: '1; mode=block' },
