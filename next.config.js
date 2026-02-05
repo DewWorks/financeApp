45fd34b10e59a4b7b52b7f81c44ab8e642b57f6b
JoaoIto <joaovictorpfr@gmail.com>
2026-02-05 16:27:40 -0300

    HOTFIX: anterior config for nextjs

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 const withSerwist = require("@serwist/next").default({
     swSrc: "src/app/sw.ts",
     swDest: "public/sw.js",
-    // Temporarily disabled for production debugging
-    disable: true, // process.env.NODE_ENV === "development",
+    disable: process.env.NODE_ENV === "development",
 });
 
 /** @type {import('next').NextConfig} */
@@ -16,26 +15,7 @@ const nextConfig = {
     webpack: (config) => {
         return config;
     },
-    trailingSlash: false,
-    async headers() {
-        return [
-            {
-                source: '/:path*',
-                headers: [
-                    { key: 'Access-Control-Allow-Credentials', value: 'true' },
-                    { key: 'Access-Control-Allow-Origin', value: '*' },
-                    { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
-                    { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
-                    { key: 'X-DNS-Prefetch-Control', value: 'on' },
-                    { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' },
-                    { key: 'X-XSS-Protection', value: '1; mode=block' },
-                    { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
-                    { key: 'X-Content-Type-Options', value: 'nosniff' },
-                ],
-            },
-        ]
-    }
-};
+}
 
 // module.exports = withSerwist(nextConfig);
 module.exports = nextConfig;
